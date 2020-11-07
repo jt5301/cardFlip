2ea753e2c2cb351b356a28c657f85010d211d404
John Tieu <johnson.tieu@gmail.com>
2020-11-07 17:35:47 -0500

    add rows and columns

--- src/App.js
+++ src/App.js
@@ -5,53 +5,68 @@ import React, {useState, useEffect} from 'react'
 function App() {
 
   const [gridNums,setGridNums] = useState({})
+  const [oddGrid,isOddGrid] = useState(false)
 
   useEffect(()=>{
     setGridNums({
       rows:4,
-      columns:3
+      columns:4
     })
   },[])
+  useEffect(()=>{
+    let nums = gridNums.columns * gridNums.rows
+    if(nums%2!=0){
+      isOddGrid(true)
+    }
+    else{
+      isOddGrid(false)
+    }
+  },[gridNums])
+
+  function modColumn(instructions){
+    let newColumns = {...gridNums}
+    if(instructions==='addColumn') newColumns.columns +=1
+    if(instructions === 'subtractColumn') newColumns.columns -=1
+    setGridNums(newColumns)
+  }
+
+  function modRow(instructions){
+    let newRows = {...gridNums}
+    if(instructions==='addRow') newRows.rows +=1
+    if(instructions === 'subtractRow') newRows.rows -=1
+    setGridNums(newRows)
+  }
+
   const buildGrid = () =>{
+    console.log(oddGrid)
     let grid = new Array(gridNums.columns).fill('')
-    grid = grid.map((column)=>{
+    grid = grid.map(()=>{
       return new Array(gridNums.rows).fill('1')
     })
     return grid.map((rows)=>{
       return (
         <div>
-        {rows.map((columns)=>{
+        {rows.map(()=>{
           return <Square/>
         })}
       </div>
       )
-
-
-
     })
-
-    // let gridColumns = new Array(columns).fill('')
-    // let grid = []
-    // for(let i = 0;i<rows;i++){
-    //   grid.push(gridColumns)
-    // }
-    // return (
-    //   <div className = 'gridContainer'>
-    //     {grid.map((rows)=>{
-    //       console.log(rows)
-    //       let count = 0
-    //       return (<Square/>)
-    //     })}
-    //   </div>
-    // )
   }
   return (
     <div className="App">
       <div className="App-body">
+      <button onClick = {()=>{
+        modColumn('addColumn')}}>Add Column</button>
+      <button onClick = {()=>{
+        modColumn('subtractColumn')}}>Remove Column</button>
       <div className = 'gridContainer'>
         {buildGrid()}
       </div>
-
+      <button onClick = {()=>{
+        modRow('addRow')}}>Add Row</button>
+      <button onClick = {()=>{
+        modRow('subtractRow')}}>Remove Row</button>
       </div>
     </div>
   );
