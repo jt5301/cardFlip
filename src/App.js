423c9b8a11dbd03215685c44707665fb7bc5873d
John Tieu <johnson.tieu@gmail.com>
2020-11-07 04:26:29 -0500

    responsive rows and columns

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square.js'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [gridNums,setGridNums] = useState({})
+
+  useEffect(()=>{
+    setGridNums({
+      rows:4,
+      columns:3
+    })
+  },[])
+  const buildGrid = () =>{
+    let grid = new Array(gridNums.columns).fill('')
+    grid = grid.map((column)=>{
+      return new Array(gridNums.rows).fill('1')
+    })
+    return grid.map((rows)=>{
+      return (
+        <div>
+        {rows.map((columns)=>{
+          return <Square/>
+        })}
+      </div>
+      )
+
+
+
+    })
+
+    // let gridColumns = new Array(columns).fill('')
+    // let grid = []
+    // for(let i = 0;i<rows;i++){
+    //   grid.push(gridColumns)
+    // }
+    // return (
+    //   <div className = 'gridContainer'>
+    //     {grid.map((rows)=>{
+    //       console.log(rows)
+    //       let count = 0
+    //       return (<Square/>)
+    //     })}
+    //   </div>
+    // )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+      <div className = 'gridContainer'>
+        {buildGrid()}
+      </div>
+
+      </div>
     </div>
   );
 }
